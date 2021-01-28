d8f602b4be1e66fe849ecf83a03ca00449b71785
GitHub Action <action@github.com>
2021-01-28 01:14:14 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/kishoretulsiani/mysecondplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.kishoretulsiani.mysecondplugin.services
+
+import com.github.kishoretulsiani.mysecondplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
